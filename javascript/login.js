697d0052d8ecc78ad04cb8434a0a15b9a0f4bc55
monaaljebzi <91837708+monaaljebzi@users.noreply.github.com>
2021-12-20 03:07:14 -0800

    Add files via upload

---
+++ javascript/login.js
@@ -0,0 +1,16 @@
+<script>
+<var modal = document.getElementById('id01');
+window.onclick = function (event) {
+if (event.target == modal) {
+	modal.style.display = "none";
+}
+}
+
+<var modal = document.getElementById('id02');
+window.onclick = function (event) {
+if (event.target == modal) {
+	modal.style.display = "none";
+}
+}
+</script>
+
